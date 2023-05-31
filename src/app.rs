5ce5fee8d7bc3528f2e4d0139238708b246af144
hritik <hchokker7@gmail.com>
2023-05-31 19:43:19 +0530

    added database configuration

--- src/app.rs
+++ src/app.rs
@@ -1,37 +1,38 @@
-use actix_web::{App, HttpServer};
-use actix_web::middleware::Logger;
-
+use actix_web::{App, HttpServer,web};
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager, Pool};
+use dotenv::dotenv;
+use std::env;
 mod routes;
 
 use routes::init_routes;
 
-pub struct AppState {
-    // Define your state here (e.g., database connection pool)
+// Function to establish a database connection pool
+fn establish_connection_pool() -> Pool<ConnectionManager<SqliteConnection>> {
+    // Load the DATABASE_URL from the .env file
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    // Create a connection manager for SQLite
+    let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create database connection pool");
+
+    pool
 }
 
 #[actix_web::main]
 pub async fn run() -> std::io::Result<()> {
-    // Create your state (e.g., initialize database connection pool)
-    let state = AppState {
-        // Initialize your state here
-    };
-    std::env::set_var("RUST_LOG", "info");
-    std::env::set_var("RUST_BACKTRACE", "1");
-    env_logger::init();
+    dotenv().ok();
+    // Initialize the database connection pool
+    let pool = establish_connection_pool();
 
+    // Start the Actix Web server and pass the database pool to the app's data
     HttpServer::new(move || {
-        let logger = Logger::default();
-        App::new().configure(init_routes).wrap(logger)
-
-        // Pass the state to the application
-        // .service(
-        //     web::scope("/")
-        //         .service(routes::todo_route::get_all_todo)
-        //         .service(routes::todo_route::create_todo)
-        //         .service(routes::todo_route::update_todo)
-        //         .service(routes::todo_route::delete_todo)
-        //         .service(routes::todo_route::get_todo_by_user), // Add more routes for other features here
-        // )
+        App::new()
+            .app_data(web::Data::new(pool.clone())) // Pass the database pool as app data
+            .configure(init_routes)
+        // ... add your routes and middleware ...
     })
     .bind("127.0.0.1:8080")?
     .run()
---
+++ src/app/db/mod.rs
@@ -0,0 +1,13 @@
+
+
+// pub fn establish_connection() -> DbPool {}
+
+
+mod config {
+    use serde::Deserialize;
+    #[derive(Debug, Default, Deserialize)]
+    pub struct ExampleConfig {
+        pub server_addr: String,
+        pub pg: deadpool_postgres::Config,
+    }
+}
